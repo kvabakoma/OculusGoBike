2eb4f8fe4deff39d8c90e92592e4a3f39797cc97
Lyubomir Rusev <lyubomir_rusev@yahoo.com>
2019-04-15 10:42:50 +0300

    Task #3: Implement component to slide the bike on the terrain.

---
+++ OculusGoBikeUnity/Assets/Scripts/GroundFollower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFollower : MonoBehaviour
+{
+	public Transform wheelFront;
+	public Transform wheelRear;
+	public float rayStartOffset = 10;
+	public float rayLength = 100;
+	public LayerMask mask;
+
+	bool GetRaycast(Vector3 position, out Vector3 hit)
+	{
+		bool result = Physics.Raycast(new Ray(position + rayStartOffset * Vector3.up, Vector3.down), out RaycastHit hitInfo, rayLength, mask);
+		hit = hitInfo.point;
+		return result;
+	}
+
+	void LateUpdate()
+    {
+		bool hitFront = GetRaycast(wheelFront.position, out Vector3 pointFront);
+		bool hitRear = GetRaycast(wheelRear.position, out Vector3 pointRear);
+
+		if(hitFront && hitRear)
+		{
+			Vector3 dir = pointFront - pointRear;
+
+			transform.LookAt(transform.position + dir, Vector3.up);
+
+			Vector3 offset = pointRear - wheelRear.position;
+			transform.position += offset;
+		}
+	}
+}
