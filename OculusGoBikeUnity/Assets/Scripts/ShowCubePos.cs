2c379ba8071ccd8f7ef38a370528b386d8cfdb64
shokiujas <49027867+shokiujas@users.noreply.github.com>
2019-04-16 12:55:45 +0300

    Updating Art Scene 3

---
+++ OculusGoBikeUnity/Assets/Scripts/ShowCubePos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowCubePos : MonoBehaviour
+{
+
+    public GameObject myCube;
+    public GameObject myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCube = GameObject.Find("Cube");
+        Debug.Log(myCube.transform.position);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.GetComponent<Text>().text = myCube.transform.localPosition.z.ToString();
+        Debug.Log(myCube.GetComponent<Transform>().position);
+    }
+}
