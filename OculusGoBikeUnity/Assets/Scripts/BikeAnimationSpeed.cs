e993b79fdd93f9d25fa6da6b2f295c82c3008fd1
Nikola Georgiev <nikola@kvaba.com>
2019-04-20 15:16:27 +0300

    Bike Animation Based on Horizontal Axis

---
+++ OculusGoBikeUnity/Assets/Scripts/BikeAnimationSpeed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeAnimationSpeed : MonoBehaviour
+{
+
+    Animator m_Animator;
+    public string axisName;
+    float axisValue;
+    public float editorDeltaSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator=gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+#if UNITY_EDITOR
+        axisValue = editorDeltaSpeed;
+#else
+		axisValue = Input.GetAxis(axisName);
+        //axisValue = .4f;
+#endif
+        m_Animator.speed = axisValue;
+    }
+}
